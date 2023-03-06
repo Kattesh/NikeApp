7302e2facdb587d00b9bf6e6eb6935b2a77be6fc
Kattesh <kate.sh@icloud.com>
2023-03-06 16:18:18 -0600

    "track order"

--- src/navigation.js
+++ src/navigation.js
@@ -4,9 +4,10 @@ import ProductDetailsScreen from "./screens/ProductDetailsScreen";
 import ShoppingCart from "./screens/ShoppingCart";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {Pressable, StyleSheet, Text} from "react-native";
-import {FontAwesome5} from "@expo/vector-icons";
+import {FontAwesome5, MaterialCommunityIcons} from "@expo/vector-icons";
 import {useSelector} from "react-redux";
 import {selectNumberOfItems} from "./store/cartSlice";
+import TrackOrder from "./screens/TrackOrder";
 
 const Stack = createNativeStackNavigator();
 
@@ -28,7 +29,14 @@ const Navigation = () => {
                                 <FontAwesome5 name="shopping-cart" size={18} color="gray"/>
                                 <Text style={styles.cartItems}> {numberOfItems} </Text>
                             </Pressable>
-                        )
+                        ),
+                        headerLeft: () => (
+                            <MaterialCommunityIcons
+                                onPress={() => navigation.navigate('Track Order')}
+                                name="truck-delivery"
+                                size={22}
+                                color="gray"/>
+                        ),
                     })}/>
                 <Stack.Screen
                     name="Product Details"
@@ -37,6 +45,9 @@ const Navigation = () => {
                 <Stack.Screen
                     name="Cart"
                     component={ShoppingCart}/>
+                <Stack.Screen
+                    name="Track Order"
+                    component={TrackOrder}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
